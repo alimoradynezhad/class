6469886cc695332046b05735e3ad5a1712e0a55e
alimoradynezhad <asmnbala@gmail.com>
2022-05-31 16:47:59 +0430

    class learn Child class

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ class person:
         self.name = name
         self.age  = age
         self.education = education
-class educ(person):
+class Child_class(person):
     pass
 
 
-person_edue = educ("reza", 15, "diplom")
+person_reza = Child_class("reza", 15, "diplom")
 person_ali = person("ali", 24, "Bachelor's degree")
 
 
-print(person_edue.education)
-print(person_edue.name)
-print(person_edue.age)
+print(person_reza.education)
+print(person_reza.name)
+print(person_reza.age)
 
 print("------------------")
 
